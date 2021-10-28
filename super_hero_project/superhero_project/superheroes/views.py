6d6ef10d95e21c9452cbc17d6c42d676cb9e8bde
Howie21 <bryce9643@gmail.com>
2021-10-28 19:01:56 -0400

    Addition of logic to edit function in sperheroes/views.py; Addition of 'edit.html' to templates

--- super_hero_project/superhero_project/superheroes/views.py
+++ super_hero_project/superhero_project/superheroes/views.py
@@ -31,3 +31,17 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    if request.method == "POST":
+        hero = Superhero.objects.get(hero_id)
+        hero.name = request.POST.get('name')
+        hero.alter_ego = request.POST.get('alter_ego')
+        hero.primary_ability = request.POST.get('primary')
+        hero.secondary_ability = request.POST.get('secondary')
+        hero.catch_phrase = request.POST.get('catch_phrase')
+        hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        hero = Superhero.objects.get(hero_id)
+        return render(request, 'superheroes/edit.html')
